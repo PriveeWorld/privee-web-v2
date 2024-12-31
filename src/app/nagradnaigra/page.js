31a2dcc94470aaced8862c54221fe6ba986715f9
Azer Basic <azerbasic@192.168.1.8>
2024-12-31 12:04:05 +0100

    Nagradna igra

--- src/app/nagradnaigra/page.js
+++ src/app/nagradnaigra/page.js
@@ -15,13 +15,13 @@ const NagradnaIgra = () => {
       text: (
         <>
           <h3
-            className="font-clashDisplay text-[40px] font-normal leading-[49.2px] tracking-[0.01em] text-left text-white mb-6"
+            className="font-clashDisplay text-[22px] sm:text-[40px] font-normal leading-[49.2px] tracking-[0.01em] text-left text-white mb-6"
             style={{
               textUnderlinePosition: "from-font",
               textDecorationSkipInk: "none",
             }}
           >
-            Podijeli svoje najbolje momente! 
+            Podijeli svoje najbolje momente!
           </h3>
           Tvoji najbolji trenuci iz 2024. godine zaslužuju pažnju i priliku da budu
           nagrađeni!   <strong
@@ -44,7 +44,7 @@ const NagradnaIgra = () => {
           Privee World je platforma koja omogućava korisnicima da na kreativan i
           organiziran način zabilježe svoje najvažnije trenutke i transformiraju ih u
           personalne dokumentarce. Revolucija je to u načinu na koji bilježimo, čuvamo i
-          dijelimo svoje uspomene – ruštveni medij budućnosti.{" "}
+          dijelimo svoje uspomene - ruštveni medij budućnosti.{" "}
           <strong
             style={{
               fontFamily: "Inter",
@@ -123,13 +123,40 @@ Obavezno zapratite naše zvanične Instagram i Facebook profile, kako biste bili
    </>) },
     {
       heading: "Kako kreirati film?",
-      text: "Privee ima tri osnovne cjeline i na svaku od njih prelazite povlačenjem (swiping) lijevo ili desno.",
+      text: (<>Privee ima tri osnovne cjeline i na svaku od njih prelazite povlačenjem (swiping) lijevo ili desno.<br></br> U nastavku, u video materijalu pogledajte kako uploadovati i kreirati svoj film   <strong
+      style={{
+        fontFamily: "Inter",
+        fontSize: "20px",
+        fontWeight: 700,
+        lineHeight: "30px",
+        letterSpacing: "0.01em",
+        textAlign: "justify",
+        textUnderlinePosition: "from-font",
+        textDecorationSkipInk: "none",
+      }}
+    >
+    #RECAP24
+    </strong></>)
     },
     {
 
-      text: (<>U nastavku, u video materijalu pogledajte kako uploadovati i kreirati svoj film #RECAP24.<br></br> <br></br>
-      Napomena: Svaki upload završava u vašem privatnom profilu. Kako biste svoj film objavili da bude vidljiv svima, potrebno je svaki video/foto/scenu pojedinačno objaviti.
-Na dnu ekrana možete pronaći ikonicu (krug) koji sadrži vodič za korištenje aplikacije. Za više informacija posjetite www.privee.world <br></br> <br></br>
+      text: (<>
+      <strong
+      style={{
+        fontFamily: "Inter",
+        fontSize: "20px",
+        fontWeight: 700,
+        lineHeight: "30px",
+        letterSpacing: "0.01em",
+        textAlign: "justify",
+        textUnderlinePosition: "from-font",
+        textDecorationSkipInk: "none",
+      }}
+    >
+    Napomena:
+    </strong> <em>Svaki upload završava u vašem privatnom profilu. Kako biste svoj film objavili da bude vidljiv svima, potrebno je svaki video/foto/scenu pojedinačno objaviti.
+Na dnu ekrana možete pronaći ikonicu (krug) koji sadrži vodič za korištenje aplikacije. Za više informacija posjetite www.privee.world</em>
+<br></br> <br></br>
 <div style={{ textAlign: "center" }}>
             <button
               onClick={() => window.open("https://priveee.onelink.me/AMM3/1313DJ", "_blank")}
@@ -157,15 +184,81 @@ Na dnu ekrana možete pronaći ikonicu (krug) koji sadrži vodič za korištenje
     },
     {
       heading: "Šta možeš osvojiti?",
-      text: "Prva nagrada: Potpuno novi iPhone 16 – zabilježi svoje buduće trenutke u vrhunskoj kvaliteti. Druga nagrada: Apple AirPods – uživaj u omiljenoj muzici uz savršeni zvuk. Neograničen broj objava za osam korsnika Privee World platforme.",
+      text: (<>    <strong
+        style={{
+          fontFamily: "Inter",
+          fontSize: "20px",
+          fontWeight: 700,
+          lineHeight: "30px",
+          letterSpacing: "0.01em",
+          textAlign: "justify",
+          textUnderlinePosition: "from-font",
+          textDecorationSkipInk: "none",
+        }}
+      >
+      Prva nagrada:
+      </strong> Potpuno novi iPhone 16 - zabilježi svoje buduće trenutke u vrhunskoj kvaliteti.<br></br> 
+      <strong
+      style={{
+        fontFamily: "Inter",
+        fontSize: "20px",
+        fontWeight: 700,
+        lineHeight: "30px",
+        letterSpacing: "0.01em",
+        textAlign: "justify",
+        textUnderlinePosition: "from-font",
+        textDecorationSkipInk: "none",
+      }}
+    >
+    Druga nagrada:
+    </strong> Apple AirPods - uživaj u omiljenoj muzici uz savršeni zvuk. Neograničen broj objava za osam korsnika Privee World platforme.</>),
     },
     {
       heading: "Važne informacije",
-      text: (<>Trajanje nagradne igre:
+      text: (<>    <strong
+        style={{
+          fontFamily: "Inter",
+          fontSize: "20px",
+          fontWeight: 700,
+          lineHeight: "30px",
+          letterSpacing: "0.01em",
+          textAlign: "justify",
+          textUnderlinePosition: "from-font",
+          textDecorationSkipInk: "none",
+        }}
+      >
+      Trajanje nagradne igre:
+      </strong>
       <br></br> Nagradna igra traje do 20. januara 2025. godine. Objavi svoj video na vrijeme kako bi imao priliku za pobjedu. <br></br> <br></br>
-Proglašenje pobjednika: <br></br>
+      <strong
+      style={{
+        fontFamily: "Inter",
+        fontSize: "20px",
+        fontWeight: 700,
+        lineHeight: "30px",
+        letterSpacing: "0.01em",
+        textAlign: "justify",
+        textUnderlinePosition: "from-font",
+        textDecorationSkipInk: "none",
+      }}
+    >
+    Proglašenje pobjednika:
+    </strong> <br></br>
 Naš stručni žiri procjenjivat će sve prijave prema sljedećim kriterijima: kreativnost, originalnost, i sposobnost da inspirišeš i zabaviš.
-Pobjednici će biti objavljeni 3. februara 2025. godine.
+<br></br>  <strong
+      style={{
+        fontFamily: "Inter",
+        fontSize: "20px",
+        fontWeight: 700,
+        lineHeight: "30px",
+        letterSpacing: "0.01em",
+        textAlign: "justify",
+        textUnderlinePosition: "from-font",
+        textDecorationSkipInk: "none",
+      }}
+    >
+   Pobjednici će biti objavljeni 3. februara 2025. godine.
+    </strong>
 <br></br> <br></br>
 Pravila i uslovi: <br></br>
 Video mora biti originalan i autentičan.
@@ -182,7 +275,30 @@ Inspiracija je ključ: Dijeli trenutke koji će pokrenuti emocije kod publike.
     },
     {
   
-      text: (<>Ovo je tvoja prilika da podijeliš svoje uspomene iz 2024. godine i osvojiš fantastične nagrade! Bilo da se radi o nevjerovatnom putovanju, posebnom životnom trenutku, ili zabavnoj situaciji koja te nasmijala do suza – tvoje priče vrijede pažnje! <br></br> <br></br> <br></br></>),
+      text: (<>Požuri! Prijave su otvorene, a tvoji najbolji trenuci čekaju da budu viđeni i nagrađeni! <br></br> <br></br>          <div style={{ textAlign: "center" }}>
+        <button
+          onClick={() => window.open("https://priveee.onelink.me/AMM3/1313DJ", "_blank")}
+          style={{
+            border: "3px solid",
+            borderImageSource: "linear-gradient(56.05deg, #CD1B70 8.3%, #F9772E 81.59%)",
+            borderImageSlice: 1,
+            padding: "8px 16px",
+            color: "#fff",
+            background: "transparent",
+            borderRadius: "10px",
+            fontFamily: "Inter",
+            fontSize: "20px",
+            fontWeight: 500,
+            lineHeight: "30px",
+            letterSpacing: "0.15em",
+            textAlign: "left",
+            textUnderlinePosition: "from-font",
+            textDecorationSkipInk: "none",
+          }}
+        >
+          FREE DOWNLOAD
+        </button>
+      </div> <br></br></>),
     },
   ];
 
@@ -195,7 +311,7 @@ Inspiracija je ključ: Dijeli trenutke koji će pokrenuti emocije kod publike.
       <TopNav onMenuClick={() => setIsNavOpen(true)}  />
 
       {/* Flex container for desktop layout */}
-      <div className="flex flex-col lg:flex-row relative h-screen w-full max-w-[1600px] overflow-y-auto px-4 py-12 sm:px-8 sm:py-16">
+      <div className="flex flex-col lg:flex-row relative mt-4 sm:mt-0 h-screen w-full max-w-[1600px] overflow-y-auto px-4 py-12 sm:px-8 sm:py-16">
         {/* Content Section */}
         <div className="flex-1">
           <AnimatePresence mode="wait">
@@ -213,7 +329,7 @@ Inspiracija je ključ: Dijeli trenutke koji će pokrenuti emocije kod publike.
                 >
                   <div className="w-full lg:w-[85%] xl:w-[95%] mx-auto px-4">
                     <h2
-                      className="font-clashDisplay text-[40px] font-semibold leading-[49.2px] tracking-[0.01em] text-left text-white mb-0.2"
+                      className="font-clashDisplay text-[28px] sm:text-[40px] font-semibold leading-[49.2px] tracking-[0.01em] text-left text-white mb-0.2"
                       style={{
                         textUnderlinePosition: "from-font",
                         textDecorationSkipInk: "none",
