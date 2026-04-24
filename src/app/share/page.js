f25a42f259cd551735f28427bf00f437a8a2c182
Azer Basic <azerbasic@users.noreply.github.com>
2026-04-24 10:28:24 +0200

    Revert "Fix social preview for pr.privee.world short links"
    This reverts commit 937e9cd9488153617420236c2a5a0d211f4e02ab.

--- src/app/share/page.js
+++ src/app/share/page.js
@@ -80,7 +80,6 @@ export async function generateMetadata({ searchParams }) {
 
   // If no data, fallback
   if (!videoData) {
-    const fallbackImage = `${domain}/images/priveelogo.png`;
     return {
       title: "Privee - Shared Video",
       description: "Check out this video on Privee!",
@@ -88,15 +87,14 @@ export async function generateMetadata({ searchParams }) {
         title: "Privee - Shared Video",
         description: "Check out this video on Privee!",
         url: `${domain}/share`,
-        siteName: "Privee World",
-        images: [{ url: fallbackImage, width: 1200, height: 630, alt: "Privee World" }],
+        images: [`${domain}/images/priveelogo.png`],
         type: "website",
       },
       twitter: {
         card: "summary_large_image",
         title: "Privee - Shared Video",
         description: "Check out this video on Privee!",
-        images: [fallbackImage],
+        images: [`${domain}/images/priveelogo.png`],
       },
     };
   }
@@ -139,9 +137,8 @@ export async function generateMetadata({ searchParams }) {
       title: metaTitle,
       description: metaDesc,
       url: fullUrl,
-      siteName: "Privee World",
       type: "video.other",
-      images: [{ url: metaImage, width: 1200, height: 630, alt: metaTitle }],
+      images: [metaImage],
     },
     twitter: {
       card: "summary_large_image",
